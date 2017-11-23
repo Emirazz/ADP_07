443a871530b013290193cf552a51b790c32b9469
Emira Zorgati <emira.zorgati97@gmx.de>
2017-11-23 19:54:25 +0100

    fertig

--- ADP_7/src/baum/ArrayBaum.java
+++ ADP_7/src/baum/ArrayBaum.java
@@ -1,18 +1,18 @@
 package baum;
 
-public class ArrayBaum<E> extends Baum<E>{
+public class ArrayBaum<E> extends Baum<E> {
 
 	private Knoten<E>[] _arr;
-	private int _size;
-	
+	private int _last;
+
 	@SuppressWarnings("unchecked")
 	public ArrayBaum() {
 		_arr = new Knoten[10];
 	}
+
 	@Override
 	public Knoten<E> getMaster() {
-		if(_arr[0] == null)
-		{
+		if (_arr[0] == null) {
 			return null;
 		}
 		return _arr[0];
@@ -22,42 +22,46 @@ public class ArrayBaum<E> extends Baum<E>{
 	@Override
 	public void add(Knoten<E> knoten) {
 		int length = _arr.length;
-		if(_size >= length){
-			Knoten<E>[] array = new Knoten[length*2];
-			for(int i = 0; i < length;i++ )
-			{
+		if (_last * 2 + 1 >= length) {
+			Knoten<E>[] array = new Knoten[length * 2];
+			for (int i = 0; i < length; i++) {
 				array[i] = _arr[i];
 			}
+			_arr = array;
 		}
-//		else if(_size == 0)
-//		{
-//			_arr[0] = knoten;
-//		}
-		else
-		{
-			_arr[getIndex(knoten, 0)] = knoten;
-		}
-		
+		// else if(_size == 0)
+		// {
+		// _arr[0] = knoten;
+		// }
+		int index = getIndex(knoten, 1);
+		_last = index;
+		_arr[index] = knoten;
+		setKind(index + 1, knoten);
 	}
-	
+
+	private void setKind(int index, Knoten<E> knoten) {
+		if (index != 1) {
+			if (index % 2 == 0) {
+				_arr[index / 2 - 1].setSohnLinks(knoten);
+			} else {
+				_arr[index / 2 - 1].setSohnRechts(knoten);
+			}
+		}
+
+	}
+
 	private int getIndex(Knoten<E> knoten, int index) {
 		int result = 0;
-		if(_arr[index] == null)
-		{
-			result = index;
-		}
-		else
-		{
-			if(_arr[index].getKey()< knoten.getKey())
-			{
-				getIndex(knoten, index*2 +1);
-			}
-			else
-			{
-				getIndex(knoten, index*2);
+		if (_arr[index - 1] == null) {
+			result = index - 1;
+		} else {
+			if (_arr[index - 1].getKey() < knoten.getKey()) {
+				result = getIndex(knoten, index * 2 + 1);
+			} else {
+				result = getIndex(knoten, index * 2);
 			}
 		}
 		return result;
 	}
-	
+
 }
---
+++ ADP_7/src/baum/BaumTest.java
@@ -0,0 +1,24 @@
+package baum;
+
+import org.junit.jupiter.api.Test;
+
+class BaumTest {
+	
+	private Baum<String> _baum;
+	
+	public BaumTest()
+	{
+		_baum = new ArrayBaum<String>();
+	}
+	@Test
+	void test() {
+		_baum.add(new Knoten<String>("b", 2));
+		_baum.add(new Knoten<String>("a", 1));
+		_baum.add(new Knoten<String>("d", 4));
+		_baum.add(new Knoten<String>("c", 3));
+		_baum.add(new Knoten<String>("f", 6));
+		_baum.add(new Knoten<String>("e", 5));
+		_baum.getInOrder();
+	}
+
+}
